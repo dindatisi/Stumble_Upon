3f9e2b2929e4f55b270d859f4d785d8fb744a6a7
dindatisi <dindatcalista@veritrans.co.id>
2018-01-27 19:04:10 +0000

    fix bug ngrams missing

--- stumble_upon.py
+++ stumble_upon.py
@@ -5,6 +5,7 @@ import nltk
 import string
 from nltk.corpus import stopwords
 from collections import Counter
+from nltk import ngrams
 
 # constant
 ignorechars = [',', '.','-','--', '&', ';', ':', '?','#','>','<','=','[',']',')','(', '|','com','html', '!']
